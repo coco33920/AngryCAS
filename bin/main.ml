9865d85a54bef75335c091194a9e4bca0ed86769
Charlotte Thomas <contact@nwa2coco.fr>
2022-03-10 21:09:48 +0100

    nevermind bugs are everywhere

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+open Angrycas.AngryCAS
+let () = print_endline "entrez une formule"
+let () = print_newline();;
+let str = read_line ();;
+let _ = string_to_parsed str  |> CAS.reduce_ast |> CAS.print_expr
--- lib/angrycas.ml
+++ lib/angrycas.ml
@@ -10,5 +10,11 @@ let lex_to_list lexbuf =
       | t -> aux (t::acc) lexbuf
   in aux [] lexbuf 
  
+  let string_to_parsed string =
+    let lexbuf = Lexing.from_string string in
+    let lexed = lex_to_list lexbuf in 
+    let parsed = Parser.parse_operation lexed
+  in parsed;;
+
 
 end
